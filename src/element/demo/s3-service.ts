d699110edb15cd0557a3198182ee8171af978b47
edeustace <ed.eustace@gmail.com>
2017-01-16 23:06:07 +0000

    stub s3 demo service

---
+++ src/element/demo/s3-service.ts
@@ -0,0 +1,31 @@
+import { PieId, DemoService as Api, DemoRouter as Router } from './service';
+import { Writable } from 'stream';
+import { readFileSync, createReadStream, ensureDirSync, createWriteStream } from 'fs-extra';
+import { dirname, join } from 'path';
+import * as express from 'express';
+import { buildLogger } from '../../log-factory';
+const logger = buildLogger();
+
+export default class S3DemoService implements Api, Router {
+
+  constructor(readonly bucket: string) {
+
+  }
+
+  stream(id: PieId, name: string): Writable {
+    return null;
+  }
+
+  prefix(): string {
+    return '?'
+  }
+
+  router(): express.Router {
+    return null;
+  }
+
+  getDemoLink(id: PieId): string {
+    return '?';
+  }
+
+}
