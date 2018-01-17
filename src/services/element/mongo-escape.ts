aa54ad4fc4d9b4dcb1209947a8f6a66e2cc8106c
edeustace <ed.eustace@gmail.com>
2018-01-17 14:19:51 -0500

    check for key

--- src/services/element/mongo-escape.ts
+++ src/services/element/mongo-escape.ts
@@ -6,16 +6,20 @@ const unescapeKey = key => key.replace('_ms_', '');
 
 export function escape(data: KeyMap): KeyMap {
   return _.reduce(data, (acc, value, key) => {
-    const finalKey = key.startsWith('$') ? escapeKey(key) : key;
-    acc[finalKey] = _.isObject(value) ? escape(value) : value;
+    if(key){
+      const finalKey = key.startsWith('$') ? escapeKey(key) : key;
+      acc[finalKey] = _.isObject(value) ? escape(value) : value;
+    }
     return acc;
   }, {});
 }
 
 export function  unescape(data: KeyMap): KeyMap {
   return _.reduce(data, (acc, value, key) => {
-    const finalKey = key.startsWith('_ms_') ? unescapeKey(key) : key;
-    acc[finalKey] = _.isObject(value) ? unescape(value) : value;
+    if(key) {
+      const finalKey = key.startsWith('_ms_') ? unescapeKey(key) : key;
+      acc[finalKey] = _.isObject(value) ? unescape(value) : value;
+    }
     return acc;
   }, {});
 }
